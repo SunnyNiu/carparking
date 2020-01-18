ca9e228442ce089a26f8801b7a770fbcdb1e3821
Sunny Niu <sunnyyingli@gmail.com>
2020-01-19 11:59:46 +1300

    refactor utility.js

--- src/functions/utility.js
+++ src/functions/utility.js
@@ -11,33 +11,17 @@ fs.readFile('src/functions/fs.txt', 'utf8', (err, data) => {
   if (err) throw err;
   const array = data.split('\n');
 
-  let obj = {};
+  const initialLocation = {};
 
-  for (let i = 0; i < array.length; i++) {
-    if (PlaceCommand.tryParse(array[i])) {
-      const command = new PlaceCommand(array[i]);
-      obj = command.execute(array[i]);
-    }
+  const commands = [PlaceCommand, TurnCommand, MoveCommand, ReportCommand];
 
-    if (TurnCommand.tryParse(array[i])) {
-      const command = new TurnCommand(array[i]);
-      obj = command.execute(obj);
-    }
-
-    if (MoveCommand.tryParse(array[i])) {
-      const command = new MoveCommand();
-      obj = command.execute(obj);
-    }
-
-    if (ReportCommand.tryParse(array[i])) {
-      const command = new ReportCommand();
-      obj = command.execute(obj);
-      const output = `${obj.x},${obj.y},${obj.facing}`;
-
-      fs.writeFile('./output.text', output, 'utf8', error => {
-        if (error) throw error;
-        console.log('Output has been saved!');
-      });
-    }
-  }
+  console.log(
+    array
+      .flatMap(str => commands.map(cmd => cmd.tryParse(str)))
+      .filter(x => x)
+      .reduce(
+        (acc, currentCommand) => currentCommand.execute(acc),
+        initialLocation
+      )
+  );
 });
